a39ed6b5c6b698838272a24544300155fa1eec2f
Lamylbill <78673748+Lamylbill@users.noreply.github.com>
2025-04-06 01:48:34 +0800

    Update checkbox.tsx

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,4 +1,3 @@
-
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
 import { Check } from "lucide-react"
@@ -12,7 +11,10 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-hrflow-blue data-[state=checked]:border-hrflow-blue data-[state=checked]:text-white",
+      "peer h-4 w-4 shrink-0 rounded-full border-2 border-gray-300 bg-white transition-colors duration-150 " +
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 " +
+      "disabled:cursor-not-allowed disabled:opacity-50 " +
+      "data-[state=checked]:bg-hrflow-blue data-[state=checked]:border-hrflow-blue data-[state=checked]:text-white",
       className
     )}
     {...props}

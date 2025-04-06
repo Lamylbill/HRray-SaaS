09a790e797b63d7facb8829a88ea9c842999882b
Lamylbill <78673748+Lamylbill@users.noreply.github.com>
2025-04-06 14:26:00 +0800

    Update checkbox.tsx

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,35 +1,35 @@
-import * as React from "react";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { cn } from "@/lib/utils";
+import * as React from "react"
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
+import { cn } from "@/lib/utils"
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
 >(({ className, ...props }, ref) => (
   <CheckboxPrimitive.Root
-  ref={ref}
-  className={cn(
-    "relative peer h-5 w-5 shrink-0 rounded-full border-2 border-gray-300 bg-white",
-    "transition-colors duration-200",
-    "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-    "disabled:cursor-not-allowed disabled:opacity-50",
-    "data-[state=checked]:bg-hrflow-blue data-[state=checked]:border-hrflow-blue",
+    ref={ref}
+    className={cn(
+      "relative peer h-5 w-5 shrink-0 rounded-full border-2 border-gray-300 bg-white",
+      "transition-colors duration-200",
+      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+      "disabled:cursor-not-allowed disabled:opacity-50",
+      "data-[state=checked]:bg-hrflow-blue data-[state=checked]:border-hrflow-blue",
       className
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator
-  className={cn(
-    "absolute inset-0 flex items-center justify-center",
-    "transition-opacity duration-200",
-    "data-[state=checked]:opacity-100 opacity-0"
-  )}
->
-  <div className="h-2.5 w-2.5 rounded-full bg-white" />
-</CheckboxPrimitive.Indicator>
+      className={cn(
+        "absolute inset-0 flex items-center justify-center",
+        "transition-opacity duration-200",
+        "data-[state=checked]:opacity-100 opacity-0"
+      )}
+    >
+      <div className="h-2.5 w-2.5 rounded-full bg-white" />
+    </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
-));
+))
 
-Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
-export { Checkbox };
+export { Checkbox }

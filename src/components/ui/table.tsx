e9d44bac8001a7cdf704d6f5d03aaec5bfca4c76
Lamylbill <78673748+Lamylbill@users.noreply.github.com>
2025-04-08 00:42:03 +0800

    Update table.tsx

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -74,8 +74,10 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-gray-700 [&:has([role=checkbox])]:pr-0",
+      "h-12 px-4 text-left align-middle font-medium text-gray-700",
       className
+)}
+
     )}
     {...props}
   />
@@ -88,7 +90,7 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle text-gray-700 [&:has([role=checkbox])]:pr-0", className)}
+    className={cn("p-4 align-middle text-gray-700", className)}
     {...props}
   />
 ))

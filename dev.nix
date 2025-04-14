b2d122254ca1487b2563195c794f28f6825b4d6f
ylbill89 <ylbill89@gmail.com>
2025-04-14 04:49:10 +0000

    updated

--- dev.nix
+++ dev.nix
@@ -5,4 +5,21 @@
     pkgs.nodePackages.vite,
     pkgs.nodePackages.npm,
   ];
+  idx.previews = {
+    previews = {
+      web = {
+        command = [
+          "npm"
+          "run"
+          "dev"
+          "--"
+          "--port"
+          "$PORT"
+          "--host"
+          "0.0.0.0"
+        ];
+        manager = "web";
+      };
+    };
+  };
 }
